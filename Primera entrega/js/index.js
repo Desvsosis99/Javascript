3cab1e0271cc468f8b4cf46277354e35786cd8e7
DiegoValdivia <diegovaldvia1@hotmail.com>
2023-11-29 23:59:22 -0500

    Ajuste variables a ENG

--- Primera entrega/js/index.js
+++ Primera entrega/js/index.js
@@ -1,36 +1,36 @@
-const productos = [
-  { nombre: "pan", precio: 3.50 },
-  { nombre: "leche", precio: 4.20 },
-  { nombre: "arroz", precio: 2.80 },
+const products = [
+  { name: "pan", price: 3.50 },
+  { name: "leche", price: 4.20 },
+  { name: "arroz", price: 2.80 },
 ];
 
-function calcularTotalConImpuesto(productosSeleccionados, impuesto) {
-  let montoTotal = 0;
-  productosSeleccionados.forEach(producto => {
-      const productoEncontrado = productos.find(p => p.nombre === producto.nombre);
-      if (productoEncontrado) {
-        montoTotal += productoEncontrado.precio * producto.cantidad;
+function calculateTotalWithTax(selectedProducts, tax) {
+  let totalAmount = 0;
+  selectedProducts.forEach(product => {
+      const foundProduct = products.find(p => p.name === product.name);
+      if (foundProduct) {
+        totalAmount += foundProduct.price * product.quantity;
       }
   });
-  return montoTotal + (montoTotal * (impuesto / 100));
+  return totalAmount + (totalAmount * (tax / 100));
 }
 
-function iniciarCalculadora() {
-  let productosSeleccionados = [];
-  let seleccionarMas = true;
+function startCalculator() {
+  let selectedProducts = [];
+  let activeSelection = true;
 
-  while (seleccionarMas) {
-      let nombreProducto = prompt("Ingrese el nombre del producto (Pan, Leche, Arroz):");
-      let cantidad = parseInt(prompt("Ingrese la cantidad del producto:"));
-      productosSeleccionados.push({ nombre: nombreProducto, cantidad });
+  while (activeSelection) {
+      let productName = prompt("Ingrese el nombre del producto (Pan, Leche, Arroz):");
+      let quantity = parseInt(prompt("Ingrese la cantidad del producto:"));
+      selectedProducts.push({ name: productName, quantity });
 
-      seleccionarMas = prompt("¿Deseas agregar otro producto? (sí/no)") === "sí";
+      activeSelection = prompt("¿Deseas agregar otro producto? (si/no)") === "si";
   }
 
-  let impuesto = parseInt(prompt("Ingresa el porcentaje de IGV (18, 10 o 0):"));
+  let tax = parseInt(prompt("Inserte el porcentaje de IGV (18, 10 o 0):"));
 
-  let montoTotal = calcularTotalConImpuesto(productosSeleccionados, impuesto);
-  alert(`En total es: S/.${montoTotal}`);
+  let totalAmount = calculateTotalWithTax(selectedProducts, tax);
+  alert(`The total is: S/.${totalAmount}`);
 }
 
-iniciarCalculadora();
+startCalculator();
