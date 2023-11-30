01461484befe36d441b557ecbdc8a64684d48e43
DiegoValdivia <diegovaldvia1@hotmail.com>
2023-11-29 23:54:04 -0500

    ajuste minusculas

--- Primera entrega/js/index.js
+++ Primera entrega/js/index.js
@@ -1,19 +1,20 @@
 const productos = [
-  { nombre: "Pan", precio: 3.50 },
-  { nombre: "Leche", precio: 4.20 },
-  { nombre: "Arroz", precio: 2.80 },
+  { nombre: "pan", precio: 3.50 },
+  { nombre: "leche", precio: 4.20 },
+  { nombre: "arroz", precio: 2.80 },
 ];
 
 function calcularTotalConImpuesto(productosSeleccionados, impuesto) {
-  let total = 0;
+  let montoTotal = 0;
   productosSeleccionados.forEach(producto => {
       const productoEncontrado = productos.find(p => p.nombre === producto.nombre);
       if (productoEncontrado) {
-          total += productoEncontrado.precio * producto.cantidad;
+        montoTotal += productoEncontrado.precio * producto.cantidad;
       }
   });
-  return total + total * (impuesto / 100);
+  return montoTotal + (montoTotal * (impuesto / 100));
 }
+
 function iniciarCalculadora() {
   let productosSeleccionados = [];
   let seleccionarMas = true;
@@ -28,8 +29,8 @@ function iniciarCalculadora() {
 
   let impuesto = parseInt(prompt("Ingresa el porcentaje de IGV (18, 10 o 0):"));
 
-  let total = calcularTotalConImpuesto(productosSeleccionados, impuesto);
-  alert(`En total es: S/.${total.toFixed(2)}`);
+  let montoTotal = calcularTotalConImpuesto(productosSeleccionados, impuesto);
+  alert(`En total es: S/.${montoTotal}`);
 }
 
 iniciarCalculadora();
