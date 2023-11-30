05dfcde52d1a878633dd625138d4840d84e956a1
DiegoValdivia <diegovaldvia1@hotmail.com>
2023-11-29 23:34:26 -0500

    Ajuste calculadora

--- Primera entrega/js/index.js
+++ Primera entrega/js/index.js
@@ -1,43 +1,35 @@
+const productos = [
+  { nombre: "Pan", precio: 3.50 },
+  { nombre: "Leche", precio: 4.20 },
+  { nombre: "Arroz", precio: 2.80 },
+];
 
-const chequeoRespuesta = (respuestaCorrecta, pregunta) => {
-    let respuesta = prompt(pregunta).toLowerCase();
-  
-    while (respuesta !== "no" && respuesta !== "si") {
-      alert("Respuesta invalida debe contestar pro si o por no");
-      respuesta = prompt(pregunta).toLowerCase();
-    }
-  
-    if (respuesta === respuestaCorrecta) {
-      alert("Respuesta correcta");
-    } else {
-      alert("Respuesta incorrecta");
-    }
-  };
+function calcularTotalConImpuesto(productosSeleccionados, impuesto) {
+  let total = 0;
+  productosSeleccionados.forEach(producto => {
+      const productoEncontrado = productos.find(p => p.nombre === producto.nombre);
+      if (productoEncontrado) {
+          total += productoEncontrado.precio * producto.cantidad;
+      }
+  });
+  return total + total * (impuesto / 100);
+}
+function iniciarCalculadora() {
+  let productosSeleccionados = [];
+  let seleccionarMas = true;
 
-  
-for (let i = 0; i <= 3; i++) {
-  let pregunta;
-  let respuestaCorrecta;
+  while (seleccionarMas) {
+      let nombreProducto = prompt("Ingrese el nombre del producto (Pan, Leche, Arroz):");
+      let cantidad = parseInt(prompt("Ingrese la cantidad del producto:"));
+      productosSeleccionados.push({ nombre: nombreProducto, cantidad });
 
-  switch (i) {
-    case 1:
-      pregunta = "Por si o por no.¿La milanesa de carne es mejor que la de pollo?";
-      respuestaCorrecta = "si";
-      chequeoRespuesta(respuestaCorrecta, pregunta);
-      break;
-    case 2:
-      pregunta = "Por si o por no.¿La milanesa es mejo con limón?";
-      respuestaCorrecta = "si";
-      chequeoRespuesta(respuestaCorrecta, pregunta);
-      break;
-    case 3:
-      pregunta = "Por si o por no.¿El fernet tiene que se Branca?";
-      respuestaCorrecta = "si";
-      chequeoRespuesta(respuestaCorrecta, pregunta);
-      break;
-    default:
-      alert("Encuesta de preguntas por si o por no");
-      break;
+      seleccionarMas = prompt("¿Deseas agregar otro producto? (sí/no)") === "sí";
   }
+
+  let impuesto = parseInt(prompt("Ingresa el porcentaje de IGV (18, 10 o 0):"));
+
+  let total = calcularTotalConImpuesto(productosSeleccionados, impuesto);
+  alert(`En total es: S/.${total.toFixed(2)}`);
 }
 
+iniciarCalculadora();
